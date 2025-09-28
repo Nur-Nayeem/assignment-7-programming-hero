2aa2f174a01078e59fe076ed9a2308068bd42eeb
nur-nayeem <nurnayem768@gmail.com>
2025-09-28 21:37:34 +0600

    modify ticket card design

--- src/components/main-section/customer-tickets/customerTicketCard/TicketCard.jsx
+++ src/components/main-section/customer-tickets/customerTicketCard/TicketCard.jsx
@@ -8,21 +8,21 @@ const TicketCard = ({ ticket, handleTaskStatus }) => {
         <div onClick={() => handleTaskStatus(ticket)} className='cursor-pointer bg-white p-4 shadow-md rounded-sm'>
             <div className='flex justify-between'>
                 <h3 className='text-lg font-medium text-[#001931]'>{title}</h3>
-                <button className='bg-[#B9F8CF] px-3 py-1 flex items-center rounded-full text-[#0B5E06]'><div className='w-[16px] h-[16px] bg-[#02A53B] rounded-full mr-1.5'></div> {status}</button>
+                <button className={`${status === 'open' ? 'bg-[#B9F8CF] text-[#0B5E06] ' : 'bg-[#F8F3B9] text-[#9C7700] '}  px-3 py-1 flex items-center rounded-full `}><div className={`w-[16px] h-[16px] ${status === 'open' ? 'bg-[#02A53B] ' : 'bg-[#FEBB0C] '}  rounded-full mr-1.5`}></div> {status}</button>
             </div>
             <p className='mt-2 mb-4 text-[#627382]'>{description}</p>
             <div className='flex justify-between items-center'>
-                <div className='flex gap-3.5'>
+                <div className='flex gap-2.5 text-sm'>
                     <span className='text-[#627382]'>#{id}</span>
-                    <span>{priority.toUpperCase()} PRIORITY</span>
+                    <span className={`${priority === 'medium' ? 'text-[#FEBB0C] ' : priority === 'high' ? 'text-[#F83044] ' : 'text-[#02A53B] '}`} >{priority.toUpperCase()} PRIORITY</span>
                 </div>
-                <div className='flex gap-3.5'>
+                <div className='flex gap-2.5 text-sm'>
                     <span className='text-[#627382]'>{customer}</span>
-                    <div className='flex gap-2.5'>
+                    <div className='flex gap-1.5'>
                         <figure>
                             <img src={calender} alt="" />
                         </figure>
-                        <span className='text-[#627382]'>{createdAt}</span>
+                        <span className='text-[#627382]'>{createdAt.replaceAll("-", "/")}</span>
                     </div>
 
                 </div>
